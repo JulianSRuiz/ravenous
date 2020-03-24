3623e2e887e9be1ae4b9fd600ba68f0c4343981b
Julian Ruiz <juliansruiz@gmail.com>
2020-03-24 16:45:13 -0700

    First commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'xBxugJQryiGAIwDCU1oVQzlPv5ri3-4XZ-diFnTLG1rEPHvvLU1KPFpCS96zum-A139VpMS0Ymet4hdxizaKon8_VABZQFEYJqsoYUEH1j37hj_9SlusfCDikEV6XnYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
